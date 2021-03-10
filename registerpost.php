e7302e3d7b355f1d96691e0a23a5ec1e5c00d56f
mdenitti <massimodn77@gmail.com>
2021-03-10 10:24:27 +0100

    Added HTML email & attachements

--- registerpost.php
+++ registerpost.php
@@ -1,5 +1,11 @@
 <?PHP
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 require 'db.php';
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
 $name = $_POST['username'];
 $password_nohash = $_POST['password'];
@@ -28,6 +34,49 @@ if (!empty($query_check_result)) {
     $query = "INSERT INTO `users` VALUES (NULL, '$name', '$password', '$target_file', '$email', 1)";
     if (mysqli_query($conn,$query)){
         echo "insert ok";
+        $mail = new PHPMailer(true);
+        try {
+            //Server settings
+           // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.mailtrap.io';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = '';                     //SMTP username
+            $mail->Password   = '';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        
+            //Recipients
+            $mail->setFrom('tiktak@syntralimburg.be', 'SyntraTikTak');
+            $mail->addAddress($email, $name);     //Add a recipient
+            //$mail->addAddress('ellen@example.com');               //Name is optional
+            //$mail->addReplyTo('info@example.com', 'Information');
+            //$mail->addCC('cc@example.com');
+            //$mail->addBCC('bcc@example.com');
+        
+            //Attachments
+            //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            $mail->addAttachment($target_file, 'uwprofiel.jpg');    //Optional name
+        
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Uw TikTakregistratie: '.$name;
+            $mail->Body    = "
+            <div style='font-family: Arial, Helvetica, sans-serif'>
+                <div style='color:red;font-size:20px'>Hallo $name</div>
+                <span style='color:black'>We hebben uw registratie goed ontvangen. U maakt gebruik van volgend e-mail adres in onze database: $email
+                </span>
+                <div>Met vriendelijke groeten...</div>
+            </div>
+            ";
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        
+            $mail->send();
+            //echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+
     } else {
         echo mysqli_error($conn);
     }
